41ad64d2afed6ada0246671bef516e1f0ab3243c
csoyn <76737270+csoyn@users.noreply.github.com>
2022-12-01 15:22:57 +0900

    Update for_FU_one_mean_table.R

--- 1_mean_table/for_FU_one_mean_table.R
+++ 1_mean_table/for_FU_one_mean_table.R
@@ -5,7 +5,7 @@ getwd()
 path <-""
 setwd(path)
 source("./0_connection.R")
-source("./1_ProcessingFunctions.R")
+source("./1_processingFunctions.R")
 source("./2_oneYearCohort.R")
 source("./3_makeMeanTable_one.R")
 dt <- read.table("./FUOneMean.txt", sep=',')
