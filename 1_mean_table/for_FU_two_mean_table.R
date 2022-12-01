a23f0fa02269d74520d0870fb1f21fde281efa03
csoyn <76737270+csoyn@users.noreply.github.com>
2022-12-01 15:23:31 +0900

    Update for_FU_two_mean_table.R

--- 1_mean_table/for_FU_two_mean_table.R
+++ 1_mean_table/for_FU_two_mean_table.R
@@ -5,7 +5,7 @@ getwd()
 path <-"E:/Users/DPSJ001/Documents/CSY/R_eAMD_0726/0726/final_codes"
 setwd(path)
 source("./0_connection.R")
-source("./1_ProcessingFunctions.R")
+source("./1_processingFunctions.R")
 source("./2_twoYearCohort.R")
 source("./3_makeMeanTable_two.R")
 
